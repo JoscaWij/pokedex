d60ff677531e344dc2aaf114d124f323394d449b
JoscaWij <Josca.W@gmx.net>
2020-08-12 16:24:14 +0200

    Add listitemicon and styling

--- src/components/ListItemIcon.js
+++ src/components/ListItemIcon.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./ListItemIcon.css";
 
 function ListItemIcon({ src, alt }) {
-  return <img src={src} alt={alt} />;
+  return <img className="listItem_icon" src={src} alt={alt} />;
 }
 
 export default ListItemIcon;
