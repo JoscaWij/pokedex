772f4af958eaa15afd082e19a0755dc7e39dd9b1
JoscaWij <Josca.W@gmx.net>
2020-08-12 12:05:30 +0200

    Add app layout

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div>Pokedex</div>;
+  return (
+    <div className="app">
+      <header className="headerSearch">
+        Pokedex
+        <input className="search" placeholder="  Enter Pokémon" />
+      </header>
+      <main className="pokemonList">Pokemons-List</main>
+      <div>Tabs</div>
+    </div>
+  );
 }
 
 export default App;
