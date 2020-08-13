a275d678b1170f767c0489635bb4ecb95ee412d0
JoscaWij <Josca.W@gmx.net>
2020-08-13 13:54:27 +0200

    Fetch api data and import it to the list with map-methode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import List from "./components/List";
 import ListItem from "./components/ListItem";
 import ListItemAvatar from "./components/ListItemAvatar";
 import ListItemText from "./components/ListItemText";
+import { fetchPokemons } from "./api/pokemon";
 
-const bulbasaur = {
+/* const bulbasaur = {
   name: "Bulbasaur",
   imgSrc: "https://img.pokemondb.net/artwork/large/bulbasaur.jpg",
   id: "001",
@@ -17,19 +18,26 @@ const ivysaur = {
   id: "002",
   link: "#ivysaur",
 };
-const allPokemons = [bulbasaur, ivysaur];
+const allPokemons = [bulbasaur, ivysaur]; */
 
 function App() {
+  const [pokemons, setPokemons] = React.useState(null);
+
+  async function handleClick() {
+    const allPokemons = await fetchPokemons();
+    setPokemons(allPokemons);
+  }
+
   return (
     <div className="app">
       <header className="headerSearch">
         Pokedex
         <input className="search" placeholder="  Enter Pokémon" />
-        <button onClick={() => alert("catch them!")}>Catch them all!</button>
+        <button onClick={handleClick}>Catch them all!</button>
       </header>
       <main className="pokemonList colorfulBorder">
         <List>
-          {allPokemons.map((pokemon) => (
+          {pokemons?.map((pokemon) => (
             <ListItem key={pokemon.id} href={pokemon.link}>
               <ListItemAvatar
                 src={pokemon.imgSrc}
