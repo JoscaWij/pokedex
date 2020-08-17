ef970f67f3dabe6d4dcc778c29bb21ec1801757b
JoscaWij <Josca.W@gmx.net>
2020-08-17 13:21:07 +0200

    Add loading and error state and response on site

--- src/pages/Pokemon.js
+++ src/pages/Pokemon.js
@@ -1,26 +1,42 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchPokemon } from "../api/pokemon";
+import LoadingScreen from "../components/LoadingScreen";
 
 function Pokemon() {
   const [pokemon, setPokemon] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const { name } = useParams();
   useEffect(() => {
     async function fetchData() {
-      const pokemonDetails = await fetchPokemon(name);
-      setPokemon(pokemonDetails);
+      try {
+        setLoading(true);
+        setError(null);
+        const pokemonDetails = await fetchPokemon(name);
+        setPokemon(pokemonDetails);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setError(error);
+      }
     }
     fetchData();
   }, [name]);
 
-  if (pokemon === null) {
-    return <div>no pokemon</div>;
+  if (error) {
+    return <div>{`You may need some help: ${error}`}</div>;
   }
+
+  if (loading || !pokemon) {
+    return <LoadingScreen />;
+  }
+
   return (
     <div>
       <span>Name: {pokemon.name}</span>
-      <span>ID: {pokemon.id}</span>
+      <div>ID: {pokemon.id}</div>
       <div>
         <img src={pokemon.imgSrc} alt={pokemon.name} />
       </div>
