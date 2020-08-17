52b686b78e44d82feb6a696c27c47c05ec0c069c
JoscaWij <Josca.W@gmx.net>
2020-08-17 09:44:37 +0200

    merge master saveing

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import List from "./components/List";
 import ListItem from "./components/ListItem";
 import ListItemAvatar from "./components/ListItemAvatar";
 import ListItemText from "./components/ListItemText";
-<<<<<<< HEAD
 import LoadingScreen from "./components/LoadingScreen";
-=======
->>>>>>> master
 import { fetchPokemons } from "./api/pokemon";
 
 /* const bulbasaur = {
@@ -26,7 +23,6 @@ const allPokemons = [bulbasaur, ivysaur]; */
 
 function App() {
   const [pokemons, setPokemons] = React.useState(null);
-<<<<<<< HEAD
   const [isLoading, setIsLoading] = React.useState(false);
 
   useEffect(() => {
@@ -70,39 +66,6 @@ function App() {
         <LoadingScreen />
       ) */}
     </>
-=======
-
-  async function handleClick() {
-    const allPokemons = await fetchPokemons();
-    setPokemons(allPokemons);
-  }
-
-  return (
-    <div className="app">
-      <header className="headerSearch">
-        Pokedex
-        <input className="search" placeholder="  Enter Pokémon" />
-        <button onClick={handleClick}>Catch them all!</button>
-      </header>
-      <main className="pokemonList colorfulBorder">
-        <List>
-          {pokemons?.map((pokemon) => (
-            <ListItem key={pokemon.id} href={pokemon.link}>
-              <ListItemAvatar
-                src={pokemon.imgSrc}
-                alt={`Picture of ${pokemon.name}`}
-              />
-              <ListItemText
-                title={pokemon.name}
-                secondarytitle={`#${pokemon.id}`}
-              />
-            </ListItem>
-          ))}
-        </List>
-      </main>
-      <footer className="bottomNav">Tabs</footer>
-    </div>
->>>>>>> master
   );
 }
 
